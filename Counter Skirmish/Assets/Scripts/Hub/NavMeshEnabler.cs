0ca9d7ec665181fcfdeea6093752c543aa3e86cc
IChainosI <tomfy45@gmail.com>
2024-04-04 16:38:19 +0200

    Terrain changes, garden arch model added

--- Counter Skirmish/Assets/Scripts/Hub/NavMeshEnabler.cs
+++ Counter Skirmish/Assets/Scripts/Hub/NavMeshEnabler.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class NavMeshEnabler : MonoBehaviour
 {
 
+    [SerializeField] GameObject navMeshCubeParent;
+
     // Start is called before the first frame update
     void Start()
     {
+
+        navMeshCubeParent.SetActive(true);
+
         foreach (Renderer renderer in gameObject.GetComponentsInChildren(typeof(Renderer)))
         {
             renderer.enabled = false;
